dcc724e91d5639428b8e01e598fc7bbcc81cfffa
zhangzheng <zzvaki@gmail.com>
2023-03-03 17:20:48 +0800

    最小化示例

--- app.vue
+++ app.vue
@@ -1,5 +1,23 @@
+<script setup lang="ts">
+import { NButton, NSpace, NGrid, NGi } from 'naive-ui';
+</script>
+
 <template>
   <div>
-    <NuxtWelcome />
+    <!-- <NuxtWelcome /> -->
+    <!-- <n-button type="error"> Error </n-button> -->
+    <n-grid x-gap="20" y-gap="36" cols="1 s:2 m:4" responsive="screen">
+      <n-gi class="item">n-gi-1</n-gi>
+      <n-gi class="item">n-gi-2</n-gi>
+      <n-gi class="item">n-gi-3</n-gi>
+      <n-gi class="item">n-gi-4</n-gi>
+    </n-grid>
   </div>
 </template>
+
+<style scoped>
+.item {
+  border: 1px salmon solid;
+  padding: 20px;
+}
+</style>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
-
-})
+  devServer: {
+    port: 5234,
+  },
+  build: {
+    transpile:
+      process.env.NODE_ENV === 'production'
+        ? [
+            'naive-ui',
+            'vueuc',
+            '@css-render/vue3-ssr',
+            '@juggle/resize-observer',
+          ]
+        : ['@juggle/resize-observer'],
+  },
+  vite: {
+    optimizeDeps: {
+      include:
+        process.env.NODE_ENV === 'development'
+          ? ['naive-ui', 'vueuc', 'date-fns-tz/esm/formatInTimeZone']
+          : [],
+    },
+  },
+});
